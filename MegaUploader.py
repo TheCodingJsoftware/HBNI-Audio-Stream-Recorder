ff8fd7f2cecafb6c1d09b631fd738fa795b9c2bc
JareBear <jaredgrozz@gmail.com>
2022-02-16 17:15:12 -0600

    bug fixed

--- MegaUploader.py
+++ MegaUploader.py
@@ -10,19 +10,6 @@ import LinksJson
 from GlobalVariables import FOLDER_LOCATION, Colors
 
 
-def main():
-    """Runs when the script first starts up"""
-    with open(f"{FOLDER_LOCATION}/credentials.json", "r") as credentialsFile:
-        credentials = json.load(credentialsFile)
-        mega = Mega({"verbose": True})
-        mega._login_user(credentials["username"], credentials["password"])
-        folder = mega.find("HBNI Audio Recording")
-        print("")
-        print(
-            f"{Colors.ENDC}{Colors.OKGREEN}Successfully logged into Mega Account{Colors.ENDC}"
-        )
-
-
 def upload(filePath: str, date: str):
     """uploades file to mega account
 
@@ -31,9 +18,15 @@ def upload(filePath: str, date: str):
             date (str): date the file was created
     """
     fileName = filePath.split("/")[-1]
-    file = mega.upload(filePath, folder[0])
-    url = mega.get_upload_link(file)
-    LinksJson.addDownloadLink(fileName=fileName, downloadLink=url, date=date)
+    with open(f"{FOLDER_LOCATION}/credentials.json", "r") as credentialsFile:
+        credentials = json.load(credentialsFile)
+        mega = Mega({"verbose": True})
+        mega._login_user(credentials["username"], credentials["password"])
+        folder = mega.find("HBNI Audio Recording")
+        print(
+            f"{Colors.ENDC}{Colors.OKGREEN}Successfully logged into Mega Account{Colors.ENDC}"
+        )
 
-
-main()
+    uploadedFile = mega.upload(filePath, folder[0])
+    downloadLink = mega.get_upload_link(uploadedFile)
+    LinksJson.addDownloadLink(fileName=fileName, downloadLink=downloadLink, date=date)
