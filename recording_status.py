2e8e544a773f9e244d25add35e8e2fbdbde44ed7
JareBear <jaredgrozz@gmail.com>
2024-05-22 13:53:01 -0500

    Update and rename RecordingStatus.py to recording_status.py

---
+++ recording_status.py
@@ -0,0 +1,15 @@
+import contextlib
+from datetime import datetime
+import json
+
+def update_recording_status(lines: list[str]):
+    data: dict[str, str] = {}
+    for line in lines:
+        host = line.split(' - ')[0]
+        descripton = line.split(' - ')[1]
+        starting_time = line.split(' - ')[2]
+        link = f"http://hbniaudio.hbni.net:8000{host}"
+        length = line.split(' - ')[3]
+        data |= {host: {"link": link, "length": length, "descripton": descripton, "starting_time": starting_time}}
+    with open(r"static\recording_status.json", "a", encoding="utf-8") as f:
+        json.dump(data, f)
