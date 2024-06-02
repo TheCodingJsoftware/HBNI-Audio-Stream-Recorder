0295e930cd979ed877102c525dc36b525cf4a116
JareBear <jaredgrozz@gmail.com>
2024-06-02 15:34:30 -0500

    miss spelled description

--- recording_status.py
+++ recording_status.py
@@ -6,10 +6,10 @@ def update_recording_status(lines: list[str]):
     data: dict[str, str] = {}
     for line in lines:
         host = line.split(' - ')[0]
-        descripton = line.split(' - ')[1]
+        description = line.split(' - ')[1]
         starting_time = line.split(' - ')[2]
         link = f"http://hbniaudio.hbni.net:8000{host}"
         length = line.split(' - ')[3]
-        data |= {host: {"link": link, "length": length, "descripton": descripton, "starting_time": starting_time}}
+        data |= {host: {"link": link, "length": length, "description": description, "starting_time": starting_time}}
     with open(r"static\recording_status.json", "w", encoding="utf-8") as f:
         json.dump(data, f)
