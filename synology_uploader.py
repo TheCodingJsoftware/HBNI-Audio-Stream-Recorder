85822aee0e7bcf812e289a9f6c768edd029424bd
Jared <jaredgrozz@gmail.com>
2025-01-03 08:51:37 -0600

    Update synology_uploader.py

--- synology_uploader.py
+++ synology_uploader.py
@@ -1,8 +1,10 @@
-import psycopg2
 import json
 import shutil
 from datetime import datetime
 from urllib.parse import quote
+
+import psycopg2
+
 
 class Colors:
     ENDC = '\033[0m'
@@ -13,10 +15,11 @@ class Colors:
 website_url = "https://audioarchives.hbni.net"
 
 db_settings = {
-    'host': "localhost",
-    'database': "postgres",
-    'user': "postgres",
-    'password': "postgres"
+    'host': "10.0.0.10",
+    'port': "5434",
+    'database': "__DATABASENAME__",
+    'user': "__USERNAME__",
+    'password': "__PASSWORD__"
 }
 
 def insert_data_to_db(file_name, download_url, date, description, length, host):
@@ -24,8 +27,8 @@ def insert_data_to_db(file_name, download_url, date, description, length, host):
     cursor = conn.cursor()
 
     insert_query = '''
-    INSERT INTO audioarchives (filename, date, description, download_link, length, host, click_count, visit_count, latest_visit, latest_click)
-    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+    INSERT INTO audioarchives (filename, date, description, download_link, length, host, visit_count, latest_visit)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
     '''
 
     cursor.execute(insert_query, (
@@ -35,10 +38,8 @@ def insert_data_to_db(file_name, download_url, date, description, length, host):
         download_url,
         length,
         host,
-        0,  # click_count initialized to 0
-        0,  # visit_count initialized to 0
-        None,  # latest_visit initialized to None
-        None  # latest_click initialized to None
+        0,
+        None,
     ))
 
     conn.commit()
@@ -50,6 +51,7 @@ def upload(file_name: str, file_path: str, host: str, description: str, date: st
         f"{Colors.ENDC}{Colors.BOLD}{datetime.now()}{Colors.ENDC} - {Colors.WARNING}Starting upload{Colors.ENDC}"
     )
 
+    file_path = file_path.replace("&amp;", "&").replace("&Amp;", "&")
     target_path = r'Z:\HBNI Audio Stream Recorder\static\Recordings'
     shutil.copy2(file_path, target_path)
 
